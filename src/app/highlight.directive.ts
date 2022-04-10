fe16f37d9ad03cefb85bcc5b9327f76f9232e8d7
Langat20 <kipkuruie7.lang@gmail.com>
2022-04-10 11:09:14 +0300

    modified the ts file

---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, HostListener, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  constructor(private elem:ElementRef) { 
+  }
+  @HostListener("click") onClicks(){
+    this.textDeco("green")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+  private textDeco(action:string){
+    this.elem.nativeElement.style.color=action;
+  }
+
+}
